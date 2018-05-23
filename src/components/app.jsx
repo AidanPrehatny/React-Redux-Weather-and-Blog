760fa56d87503a18b326591ae3d0c03918937e67
Enomer <aprehatny@yahoo.com>
2018-05-22 23:53:06 -0700

    intial commit w/ boilerplate

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+import Searchbar from '../containers/search_bar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Searchbar />
+      </div>
+    );
+  }
+}

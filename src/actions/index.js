c73425ecd17f62bf65f1fdaca3d5917aad8ad586
Enomer <aprehatny@yahoo.com>
2018-05-27 04:56:49 -0700

    created axios delete action and added Delete button to each blog post | delete button pushes back to /posts with router context

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,12 @@ const WEATHER_API_KEY = 'a9970cc48b5382dbd85c873513e7014f';
 const WEATHER_ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?&APPID=${WEATHER_API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-const BLOG_API_KEY = '?key=prehot18';
+const BLOG_API_KEY = '?key=asdf';
 const BLOG_ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST'
 
 export const LAT_LON = 'LAT_LON';
 
@@ -45,6 +46,15 @@ export function fetchPost(id) {
 
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${BLOG_ROOT_URL}/posts/${id}${BLOG_API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
--- src/components/blog/posts_show.jsx
+++ src/components/blog/posts_show.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../../actions/index';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
+
+import { fetchPost, deletePost } from '../../actions/index';
 
 class PostsShow extends Component {
 
   componentWillMount() {
     this.props.fetchPost(this.props.match.params.id)
+  }
+
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.match.params.id)
+    .then(()=> {
+      this.context.router.history.push('/posts')
+    })
   }
 
   render() {
@@ -16,9 +30,16 @@ class PostsShow extends Component {
     return (
       <div>
         <br></br>
+        <Link to="/posts"><button className="btn btn-primary">Back</button></Link>
+        <button
+          className="btn btn-danger float-right"
+          onClick={this.onDeleteClick.bind(this)}
+          >
+          Delete Post
+        </button>
         <hr></hr>
-        <h3>{post.title}</h3>
-        <h6>Categories: {post.categories}</h6>
+        <h2>{post.title}</h2>
+        <h5>Categories: {post.categories}</h5>
         <p>{post.content}</p>
       </div>
     )
@@ -29,4 +50,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow)
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow)
